af2225c5fc7f5e1fbefe54964e1a3edcc174540b
Arslan Ali <arxlanali@gmail.com>
2022-08-19 17:45:37 +0500

    This commit is for the timer with multiple clocks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function HelloWorld() {
-    return (
-        <div>
-            <Hello /> <World />
-        </div>
-    );
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+function Clock(props) {
+  const [time, setTime] = useState();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleString());
+    }, props.interval);
+    return () => {
+      clearInterval(timer);
+    };
+  });
+  //console.log(time);
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {time}.</h2>
+    </div>
+  );
+}
+function MultipleClock(props) {
+  return (
+    <div>
+      <Clock interval={1000} />
+      <Clock interval={2000} />
+    </div>
+  );
 }
 
-function Hello() {
-    return (
-        <span>Hello</span>
-    );
-}
-function World() {
-    return (
-        <span>world</span>
-    );
-}
-ReactDOM.render(
-    <HelloWorld />,
-    document.querySelector('#root')
-);
-
-
-
+const root = createRoot(document.getElementById("root"));
+root.render(<MultipleClock />);
